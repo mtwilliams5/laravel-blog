fe17137e48a05ade4c033f18c520a5651234c0c3
Matt Williams <matt@mtwilliams.uk>
2017-08-21 17:54:18 +0100

    Stripping the Editor's H1 tag from the title field

--- app/Http/Middleware/AlwaysClean.php
+++ app/Http/Middleware/AlwaysClean.php
@@ -16,10 +16,17 @@ class AlwaysClean
      */
     public function handle($request, Closure $next)
     {
+        // First let's clean the content field of any nasty code
         if ($request->input('content')) {
             $content = $request->input('content');
             $cleanContent = Purifier::clean($content);
             $request->merge(['content' => $cleanContent]);
+        }
+        // Now let's strip the title of any HTML, so our fancy editor doesn't mess up our display
+        if ($request->input('title')) {
+            $title = $request->input('title');
+            $bareTitle = strip_tags($title);
+            $request->merge(['title' => $bareTitle]);
         }
         return $next($request);
     }
